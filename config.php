0423cab8c94750b293876e9a60a997ce09cdd474
Deep Toaster <deeptoaster@gmail.com>
2016-01-08 18:34:29 -0800

    added initial pages

--- config.php
+++ config.php
@@ -5,4 +5,12 @@ $config['table_quotes'] = 'quotes';
 $config['table_votes'] = 'votes';
 $config['template_path'] = 'templates/default';
 $config['title'] = 'Hell Quotes';
+$config['page_titles']['home'] = 'Home';
+$config['page_titles']['latest'] = 'Latest';
+$config['page_titles']['browse'] = 'Browse';
+$config['page_titles']['random'] = 'Random';
+$config['page_titles']['top'] = 'Top';
+$config['page_titles']['add'] = 'Add Quote';
+$config['page_titles']['admin'] = 'Admin';
+$config['page_titles']['search'] = 'Search';
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?
-$config = array();
+$config = array(
+	'page_titles' => array()
+);
 
 include('config.php');
 include('lib/cleverly/Cleverly.class.php');
 
+if (@$_GET['page'] == 'home') {
+	header('Location: ./', true, 301);
+}
+
 $create = !file_exists($config['db_path']);
 $pdo = new PDO('sqlite:' . $config['db_path']);
 $cleverly = new Cleverly();
+$cleverly->setTemplateDir(__DIR__ . '/' . $config['template_path']);
 
 if ($create) {
 	$pdo->exec(<<<EOF
@@ -31,5 +38,34 @@ EOF
 		);
 }
 
-$cleverly->display(__DIR__ . "/$config[template_path]/index.tpl", $config);
+$cleverly->display('index.tpl', $config + array(
+	'page_title' => 'Yet Another Quote Database',
+	'nav' => function() {
+		global $cleverly;
+		global $config;
+
+		foreach ($config['page_titles'] as $page => $title) {
+			$vars = array(
+				'link_name' => $page,
+				'link_title' => $title
+			);
+
+			if ($page == 'home' and !isset($_GET['page']) or $page == @$_GET['page']) {
+				$cleverly->display('nav_link_active.tpl', $vars);
+			} else {
+				$cleverly->display('nav_link.tpl', $vars);
+			}
+		}
+	},
+	'main' => function() {
+		global $cleverly;
+		global $config;
+
+		switch (@$_GET['page']) {
+			case '':
+				$cleverly->display('main_default.tpl');
+				break;
+		}
+	}
+));
 ?>
