3926575a5f79e32badb2b0a187f12f7e45e0baaa
Deep Toaster <deeptoaster@gmail.com>
2016-01-18 11:20:22 -0800

    added pager and SQL code

--- index.php
+++ index.php
@@ -6,6 +6,37 @@ $config = array(
 include('config.php');
 include('lib/cleverly/Cleverly.class.php');
 
+function generate_link($section, $src, $dest) {
+	global $cleverly;
+
+	return $cleverly->fetch($src == $dest ? 'pager_link_active.tpl' : 'pager_link.tpl', array(
+		'link_index' => $dest,
+		'link_url' => generate_url($section, $dest);
+	));
+}
+
+function generate_url($section = NULL, $index = NULL) {
+	if ($section == 'home') {
+		$section = NULL;
+	}
+
+	if ($_config['pretty_url']) {
+
+	} else {
+		$url = './';
+
+		if ($section) {
+			$url .= '?page=' . $section;
+		}
+
+		if ($index) {
+			$url .= ($section ? '&' : '?') . 'p=' . $index;
+		}
+
+		return $url;
+	}
+}
+
 if (@$_GET['page'] == 'home') {
 	header('Location: ./', true, 301);
 }
@@ -39,24 +70,77 @@ EOF
 		);
 }
 
+$order = '';
+$section = @$_GET['page'] ? $_GET['page'] : 'home';
+
+if (!isset($config[$section])) {
+	$section = 'error';
+}
+
+switch ($section) {
+	case 'latest':
+		$order = 'ORDER BY `created` DESC';
+		break;
+	case 'browse':
+		$order = 'ORDER BY `created`';
+		break;
+	case 'random':
+		$order = 'ORDER BY RANDOM()';
+		break;
+	case 'top':
+		$order = 'ORDER BY `score`';
+		break;
+}
+
+$limit = 'LIMIT ' . $config['posts_per_page'];
+$index = (int) @$_GET['p'];
+$offset = 'OFFSET ' . $index * $config['posts_per_page'];
+$nav = '';
+
+foreach ($config['page_titles'] as $page => $title) {
+	$vars = array(
+		'link_name' => $page,
+		'link_title' => $title,
+		'link_url' => generate_url($page);
+	);
+
+	$nav .= $cleverly->fetch($page == $section ? 'nav_link_active.tpl' : 'nav_link.tpl', $vars);
+}
+
+$result = $pdo->prepare(<<<EOF
+SELECT COUNT(*)
+FROM `$config[table_quotes]`
+EOF
+	);
+
+$result->execute();
+$num_index = $result->fetchColumn();
+$min_index = max(1, $index - 5);
+$max_index = min($num_index - 2, $index + 5);
+$pager = generate_link($section, $index, 0);
+
+if ($index > 6) {
+	$pager .= ' &ellipses; ';
+}
+
+
+for ($i = $min_index; $i <= $max_index; $i++) {
+	$pager .= generate_link($section, $index, $i);
+}
+
+if ($index < $num_index - 7) {
+	$pager .= ' &ellipses; ';
+}
+
+$pager .= generate_link($section, $index, $num_index - 1);
+
 $cleverly->display('index.tpl', $config + array(
-	'page_title' => 'Yet Another Quote Database',
+	'page_title' => @$config['page_titles'][$_GET['page']],
+	'pager' => function() {
+		echo $pager;
+	},
 	'nav' => function() {
-		global $cleverly;
-		global $config;
-
-		foreach ($config['page_titles'] as $page => $title) {
-			$vars = array(
-				'link_name' => $page,
-				'link_title' => $title
-			);
-
-			if ($page == 'home' and !isset($_GET['page']) or $page == @$_GET['page']) {
-				$cleverly->display('nav_link_active.tpl', $vars);
-			} else {
-				$cleverly->display('nav_link.tpl', $vars);
-			}
-		}
+		echo $nav;
 	},
 	'main' => function() {
 		global $cleverly;
@@ -66,17 +150,13 @@ $cleverly->display('index.tpl', $config + array(
 			case '':
 				$cleverly->display('main_home.tpl');
 				break;
-			case 'latest':
-				$cleverly->display('main_latest.tpl');
-				break;
 			case 'browse':
-				$cleverly->display('main_browse.tpl');
+				$cleverly->display('main_paged.tpl');
 				break;
+			case 'latest':
 			case 'random':
-				$cleverly->display('main_random.tpl');
-				break;
 			case 'top':
-				$cleverly->display('main_top.tpl');
+				$cleverly->display('main_unpaged.tpl');
 				break;
 			case 'add':
 				$cleverly->display('main_add.tpl');
--- templates/default/nav_link.tpl
+++ templates/default/nav_link.tpl
@@ -1,3 +1,3 @@
 <li>
-	<a href="?page={$link_name}">{$link_title}</a>
+	<a href="{$link_url}">{$link_title}</a>
 </li>
